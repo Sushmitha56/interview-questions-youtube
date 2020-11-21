b14fe61263c55fb5b5bc6db3e31b38c06846cf83
Sushmitha56 <65645807+Sushmitha56@users.noreply.github.com>
2020-11-21 15:31:29 +0530

    Update syntax.js

--- junior_dev_questions/syntax.js
+++ junior_dev_questions/syntax.js
@@ -1,13 +1,9 @@
 function foo1() {
-    return {
-        bar: "hello"
-    };
+    return { bar: "hello" };
 }
 
 function foo2() {
-    return{
-        bar: "hello"
-    };
+    return {bar: "hello"};
 }
 
 console.log(foo1());
